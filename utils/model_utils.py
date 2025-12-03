c56256e1876c8cacf106b35dd3c7de359630e318
FIXED-TERM Tran Khoi Nguyen (MS/EAA45-VM) <rga6hc@bosch.com>
2025-12-03 14:51:43 +0700

    Update warmup

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -28,8 +28,8 @@ def lr_lambda_update_epoch(current_epoch, cfg):
     lr_epoch_step_size = cfg["lr_epoch_step_size"]
 
     if current_epoch < warmup_epochs:
-        alpha = float(current_epoch) / float(max(1, warmup_epochs))
-        return warmup_factor * (1.0 - alpha) + alpha
+        return = float(current_epoch) / float(max(1, warmup_epochs))
+        
     else:
         num_decays = (current_epoch - warmup_epochs) // lr_epoch_step_size
         return decay_factor ** (num_decays)
